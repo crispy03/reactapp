f9cb29bf9e8a543e19cd7da47f3a3a4ff2065fbd
crispy03 <wukongking16@gmail.com>
2020-09-02 20:27:34 +0500

    Updates

---
+++ precache-manifest.9bb333505d6eff76a92e3809dd5b80de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4915e0a8830e2119431a5d7f58283ac3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "71092b7417cf172146eb",
+    "url": "/react-tutorial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e10131e1587f687f514b",
+    "url": "/react-tutorial/static/js/2.b3dc571a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.b3dc571a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71092b7417cf172146eb",
+    "url": "/react-tutorial/static/js/main.01051ce5.chunk.js"
+  },
+  {
+    "revision": "6e3d92b936c35cd1b7d3",
+    "url": "/react-tutorial/static/js/runtime-main.a688c7a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
